f4db3402e07a2c07c276b8393d2afe34ec648e7b
rasyidannas <rasshit.dsgn@gmail.com>
2025-08-10 12:08:38 +0000

    sturcture

--- exercise.lean
+++ exercise.lean
@@ -8,3 +8,30 @@ def volRectanglePrism (height : Nat) (weight : Nat) (depth : Nat) : Nat :=
   height * weight * depth
 
 #eval volRectanglePrism 4 2 3
+
+structure RectangularPrism where
+  height : Float
+  width : Float
+  depth : Float
+deriving Repr
+
+def volume (p : RectangularPrism) : Float :=
+  p.height * p.width * p.depth
+
+#eval volume { height := 2.0, width := 3.0, depth := 3.0 }
+
+structure Segment where
+  p1 : (Float × Float)  -- This is tuples, first endpoint (x₁, y₁)
+  p2 : (Float × Float)  -- THis is tuples, second endpoint (x₂, y₂)
+deriving Repr
+
+def seg : Segment := {p1 := (0.0, 0.0), p2 := (3.0, 4.0)}
+#eval seg.p1
+
+def length (s : Segment) : Float :=
+  let (x1, y1) := s.p1
+  let (x2, y2) := s.p2
+  Float.sqrt ((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
+
+#eval length { p1 := (0.0, 0.0), p2 := (3.0, 4.0) }
+
